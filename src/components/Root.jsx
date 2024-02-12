bad8f5f69ca13f630d94a31d22d439e0aff7db88
Bablu Mehta <babloomehta895012@gmail.com>
2024-02-12 10:13:43 +0530

    MUI icon is installed

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import SideMenu from "./SideMenu";
 
 const Root = () => {
   return (
     <>
-      <div>Root</div>
+      <SideMenu />
       <Outlet />
     </>
   );
