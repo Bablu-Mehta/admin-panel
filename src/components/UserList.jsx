892e173f1e776a217baa7601fcc878782f1be86b
Bablu Mehta <babloomehta895012@gmail.com>
2024-02-12 14:05:40 +0530

    table layout is complete

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -6,30 +6,28 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { Button } from "@mui/material";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, userName, email) {
+  return { name, userName, email };
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Bablu", "bablu123", "bablu123@gmail.com"),
+  createData("Sham", "sham123", "sham123@gmail.com"),
+  createData("rahul", "rahul123", "rahul123@gmail.com"),
+  createData("Bablu", "bablu123", "bablu123@gmail.com"),
 ];
 
 const UserList = () => {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table  sx={{ minWidth: 650, maxWidth: 1200 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">UserName</TableCell>
+            <TableCell align="right">Email</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -41,10 +39,18 @@ const UserList = () => {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.userName}</TableCell>
+              <TableCell align="right">{row.email}</TableCell>
+              <TableCell align="right">
+                <Button variant="contained" color="success">
+                  Details
+                </Button>
+              </TableCell>
+              <TableCell align="right">
+                <Button variant="outlined" color="error">
+                  Delete
+                </Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
