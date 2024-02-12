b8d03f31ef4f23914217fa56a9043f2b79faef96
Bablu Mehta <babloomehta895012@gmail.com>
2024-02-12 10:02:41 +0530

    index Router is and rootLayout component is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import Root from "./components/Root";
+
+const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
 
 function App() {
-
-  return <>Admin Panel</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
