3c2393497c96b2ebe176a6f6e507ce222747229d
Bablu Mehta <babloomehta895012@gmail.com>
2024-02-14 10:42:01 +0530

    form created and styling is almost done and the react-hook-form yup and @hookform/resolvers are installed as dependencies

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -6,18 +6,20 @@ import { modalActions } from "../store/modal-slice";
 // import { redirect } from "react-router-dom";
 import { Stack } from "@mui/material";
 
+import classes from "./CreateUser.module.css";
+
 const CreateUser = () => {
   const open = useSelector((state) => state.modal.isOpen);
   const close = useSelector((state) => state.modal.isClose);
 
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const openModal = () => {
-      dispatch(modalActions.openModal());
-    };
-    openModal();
-  }, []);
+  // useEffect(() => {
+  //   const openModal = () => {
+  //     dispatch(modalActions.openModal());
+  //   };
+  //   openModal();
+  // }, []);
 
   // const handleCloseModal = () => {
   //   dispatch(modalActions.closeModal());
@@ -32,14 +34,37 @@ const CreateUser = () => {
   };
 
   return (
-    <ModalUI isOpen={open} onClose={close}>
-      <Stack>
-        <Button variant="contained" onClick={handleSubmit}>
-          close
-        </Button>
-      </Stack>
-      {/* <button>close</button>   */}
-    </ModalUI>
+    // <ModalUI isOpen={open} onClose={close}>
+    //   <Stack>
+    //     <Button variant="contained" onClick={handleSubmit}>
+    //       close
+    //     </Button>
+    //   </Stack>
+    //   {/* <button>close</button>   */}
+    // </ModalUI>
+
+    <div className={classes.container}>
+      <form className={classes.form}>
+        <div className={classes.inputContainer}>
+          <label htmlFor="name">Enter Your Name</label>
+          <input type="text" name="name" />
+        </div>
+
+        <div className={classes.inputContainer}>
+          <label htmlFor="username">Enter Your Username</label>
+          <input type="text" name="name" />
+        </div>
+
+        <div className={classes.inputContainer}>
+          <label htmlFor="email">Enter Your Email</label>
+          <input type="text" name="email" />
+        </div>
+        <div className={classes.buttonContainer}>
+          <button type="button">Cancel</button>
+          <button type="submit">Submit Form</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
